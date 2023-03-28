c4f902d5d032a511255dc32b8f5d07efb6444bbc
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-03-27 21:34:40 -0300

    first commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemplosExplorando.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        public string Nome
+        {
+            get => _nome.ToUpper();
+
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
+
+        }
+
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using ExemplosExplorando.Models;
+
+
+Pessoa pessoa1 =  new Pessoa();
+
+pessoa1.Nome = "Jorgin";
+pessoa1.Idade = 23 ;
+pessoa1.Apresentar();
+
+ 
