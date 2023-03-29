04c8b5e045b2fc32e91802de333adfd774a3694f
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-03-28 23:44:57 -0300

    feat: fooi feito a instancia da classe curso adicionando alunos a lista

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
 ﻿using ExemplosExplorando.Models;
 
 
-Pessoa pessoa1 =  new Pessoa();
+Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome = "Jorgin  ";
-pessoa1.Idade = 0 ;
+pessoa1.Nome = "Jorgin";
+pessoa1.Idade = 23;
+
 pessoa1.Apresentar();
 
- 
+
+
+ Pessoa pessoa2 = new Pessoa();
+ pessoa2.Nome = "Galeguin Safado";
+ pessoa2.Idade = 23; 
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+cursoDeIngles.adicionarAluno(pessoa1);
+cursoDeIngles.adicionarAluno(pessoa2);
+cursoDeIngles.ListarAlunos();
+
+
+
